9a411f594b1b7d9ff8b08392701e0b0cf7616936
Mariana Santana <sansilmari2@gmail.com>
2020-04-19 19:27:43 -0300

    add day11

---
+++ Day11-2dArrays.kt
@@ -0,0 +1,41 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+
+    var highestSum = 0
+
+    for (i in 0 until 6) {
+        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    }
+
+    for (i in 0 until 4) {
+        for (j in 0 until 4) {
+            val aux = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+
+            if(highestSum < aux) highestSum = aux
+        }
+    }
+
+    print(highestSum)
+
+}
